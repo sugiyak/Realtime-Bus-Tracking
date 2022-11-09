d094156955018c1f942e49a155fc83242dcfeea4
杉山慶 <sugiyamakei@sugiyamakeinoMacBook-Pro.local>
2022-11-09 15:35:10 -0800

    testing accesstoken

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,13 +3,13 @@ const { merge } = require('webpack-merge'); // webpack-merge
 const common = require('./webpack.common.js'); // import webpack.common.js
 const webpack = require("webpack");
 
-
+console.log(process.env.ACCESSTOKEN)
 module.exports = merge(common, {
     mode: "production",
     plugins: [
         new webpack.DefinePlugin({
             'process.env': {
-              'ACCESSTOKEN': JSON.stringify(process.env.YOUR_VARIABLE),
+              'ACCESSTOKEN': JSON.stringify(process.env.ACCESSTOKEN),
             }
           })
         ],
