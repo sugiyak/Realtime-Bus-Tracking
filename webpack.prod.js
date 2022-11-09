3df7333ee8ff2130e41dada8e591f92943808b5c
杉山慶 <sugiyamakei@sugiyamakeinoMacBook-Pro.local>
2022-11-09 15:49:08 -0800

    fixed webpack.prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -9,7 +9,7 @@ module.exports = merge(common, {
     plugins: [
         new webpack.DefinePlugin({
             'process.env': {
-              'ACCESSTOKEN': JSON.stringify(process.env.ACCESSTOKEN),
+              'ACCESSTOKEN': process.env.ACCESSTOKEN,
             }
           })
         ],
